ea825f6d0b9fe1a94f763be9c855b6a4f5e1bcf2
Mr.Hope <zhangbowang1998@gmail.com>
2020-01-19 12:15:32 +0800

    Revert restaurant.js

--- app/pages/views/restaurant.js
+++ app/pages/views/restaurant.js
@@ -1,91 +1,75 @@
-const app = getApp();
 Page({
   data: {
     List: [],
     load: true,
     restaurant: ''
   },
-  onLoad(e) {
-    this.setData({
-      restaurant: e.restaurant
-    });
-    const that = this;
+
+  onLoad(event) {
+    console.log(event);
+    this.setData({ restaurant: event.restaurant });
+
     wx.showLoading({
       title: '加载中...',
       mask: true
     });
+
     wx.request({
       url: 'https://lin.innenu.com/query-restaurant.php',
-      data: {
-        restaurant: e.restaurant
-      },
-      success(res) {
+      data: { restaurant: event.restaurant },
+      success: res => {
         console.log(res);
-        const list = res.data;
-        that.setData({
-          List: list
-        });
+        const List = res.data;
+        this.setData({ List });
       }
     });
   },
-  goBack() {
-    wx.navigateBack({
-      delta: 1
-    });
-  },
-  goBackToHome() {
-    wx.navigateBack({
-      delta: 100
-    });
-  },
-  serach(e) {
-    wx.redirectTo({
-      url: '/pages/detail/detail'
-    });
-  },
+
   onReady() {
     wx.hideLoading();
   },
-  tabSelect(e) {
+
+  serach() {
+    wx.redirectTo({ url: '/pages/detail/detail' });
+  },
+
+  tabSelect(event) {
+    const { id } = event.currentTarget.dataset;
+
     this.setData({
-      TabCur: e.currentTarget.dataset.id,
-      MainCur: e.currentTarget.dataset.id,
-      VerticalNavTop: (e.currentTarget.dataset.id - 1) * 50
+      TabCur: id,
+      MainCur: id,
+      VerticalNavTop: (id - 1) * 50
     });
   },
-  VerticalMain(e) {
-    const that = this;
+  verticalMain(event) {
     const { list } = this.data;
     let tabHeight = 0;
+
     if (this.data.load) {
       for (let i = 0; i < list.length; i++) {
         const view = wx.createSelectorQuery().select(`#main-${list[i].id}`);
+
         view
-          .fields(
-            {
-              size: true
-            },
-            data => {
-              list[i].top = tabHeight;
-              tabHeight += data.height;
-              list[i].bottom = tabHeight;
-            }
-          )
+          // eslint-disable-next-line no-loop-func
+          .fields({ size: true }, data => {
+            list[i].top = tabHeight;
+            tabHeight += data.height;
+            list[i].bottom = tabHeight;
+          })
           .exec();
       }
-      that.setData({
-        load: false,
-        list
-      });
+
+      this.setData({ load: false, list });
     }
-    const scrollTop = e.detail.scrollTop + 20;
+
+    const scrollTop = event.detail.scrollTop + 20;
+
     for (let i = 0; i < list.length; i++)
-      if (scrollTop > list[i].top && scrollTop < list[i].bottom) {
-        that.setData({
+      if (scrollTop > list[i].top && scrollTop < list[i].bottom)
+        this.setData({
           VerticalNavTop: (list[i].id - 1) * 50,
           TabCur: list[i].id
         });
-        return false;
-      }
   }
 });
