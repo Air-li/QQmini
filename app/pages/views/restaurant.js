df9be047d2b9e9522728d25d2589db1bf3034fb2
Yaoyi <13110235070@163.com>
2020-01-19 11:57:12 +0800

    test: try push

--- app/pages/views/restaurant.js
+++ app/pages/views/restaurant.js
@@ -1,75 +1,91 @@
+const app = getApp();
 Page({
   data: {
     List: [],
     load: true,
     restaurant: ''
   },
-
-  onLoad(event) {
-    console.log(event);
-    this.setData({ restaurant: event.restaurant });
-
+  onLoad(e) {
+    this.setData({
+      restaurant: e.restaurant
+    });
+    const that = this;
     wx.showLoading({
       title: '加载中...',
       mask: true
     });
-
     wx.request({
       url: 'https://lin.innenu.com/query-restaurant.php',
-      data: { restaurant: event.restaurant },
-      success: res => {
+      data: {
+        restaurant: e.restaurant
+      },
+      success(res) {
         console.log(res);
-        const List = res.data;
-        this.setData({ List });
+        const list = res.data;
+        that.setData({
+          List: list
+        });
       }
     });
   },
-
+  goBack() {
+    wx.navigateBack({
+      delta: 1
+    });
+  },
+  goBackToHome() {
+    wx.navigateBack({
+      delta: 100
+    });
+  },
+  serach(e) {
+    wx.redirectTo({
+      url: '/pages/detail/detail'
+    });
+  },
   onReady() {
     wx.hideLoading();
   },
-
-  serach() {
-    wx.redirectTo({ url: '/pages/detail/detail' });
-  },
-
-  tabSelect(event) {
-    const { id } = event.currentTarget.dataset;
-
+  tabSelect(e) {
     this.setData({
-      TabCur: id,
-      MainCur: id,
-      VerticalNavTop: (id - 1) * 50
+      TabCur: e.currentTarget.dataset.id,
+      MainCur: e.currentTarget.dataset.id,
+      VerticalNavTop: (e.currentTarget.dataset.id - 1) * 50
     });
   },
-  verticalMain(event) {
+  VerticalMain(e) {
+    const that = this;
     const { list } = this.data;
     let tabHeight = 0;
-
     if (this.data.load) {
       for (let i = 0; i < list.length; i++) {
         const view = wx.createSelectorQuery().select(`#main-${list[i].id}`);
-
         view
-          // eslint-disable-next-line no-loop-func
-          .fields({ size: true }, data => {
-            list[i].top = tabHeight;
-            tabHeight += data.height;
-            list[i].bottom = tabHeight;
-          })
+          .fields(
+            {
+              size: true
+            },
+            data => {
+              list[i].top = tabHeight;
+              tabHeight += data.height;
+              list[i].bottom = tabHeight;
+            }
+          )
           .exec();
       }
-
-      this.setData({ load: false, list });
+      that.setData({
+        load: false,
+        list
+      });
     }
-
-    const scrollTop = event.detail.scrollTop + 20;
-
+    const scrollTop = e.detail.scrollTop + 20;
     for (let i = 0; i < list.length; i++)
-      if (scrollTop > list[i].top && scrollTop < list[i].bottom)
-        this.setData({
+      if (scrollTop > list[i].top && scrollTop < list[i].bottom) {
+        that.setData({
           VerticalNavTop: (list[i].id - 1) * 50,
           TabCur: list[i].id
         });
+        return false;
+      }
   }
 });
